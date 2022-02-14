a019ceb8afb2fe9b653641941cdae6bbe063760c
Edwaldo Utama <edwaldo.utama22@yahoo.co.id>
2022-02-14 19:33:11 +0700

    fix
    fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Contact = require('./model/contact');
 const { Mongoose } = require('mongoose');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //setup method override
 app.use(methodOverride('_method'));
